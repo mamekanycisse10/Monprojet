0a6e6a8157b3857925f000c7239b5fb0c498f14b
mamekanycisse10E <mamekany.cisse@unchk.edu.sn>
2026-03-02 12:53:56 +0000

    Premier commit

---
+++ src/main/java/com/mame/monprojet/MonprojetApplication.java
@@ -0,0 +1,13 @@
+package com.mame.monprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonprojetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonprojetApplication.class, args);
+    }
+
+}
